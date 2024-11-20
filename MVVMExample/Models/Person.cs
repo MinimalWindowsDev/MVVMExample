8be1c735bf3d88b97c3a42762a56eb68af843fcf
F. S. Farimani <foad.farimani@nts-group.nl>
2024-11-20 16:29:34 +0100

    feat: implement MVVM architecture for WPF application
    - Create basic MVVM folder structure (Models, Views, ViewModels)
    - Implement Person model class for data structure
    - Add ViewModelBase class implementing INotifyPropertyChanged
    - Create RelayCommand class for handling command pattern
    - Implement MainViewModel with ObservableCollection and commands
    - Add MainWindow XAML view with data bindings
    - Configure project for .NET Framework 4.8 with WPF dependencies
    - Set correct build actions and startup configurations
    - Fix resource location issues for MainWindow.xaml
    
    The application now demonstrates core MVVM concepts including:
    - Separation of concerns
    - Data binding
    - Command pattern
    - Property change notification
    - Collection management

--- MVVMExample/Models/Person.cs
+++ MVVMExample/Models/Person.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MVVMExample.Models
+﻿namespace MVVMExample.Models
 {
-    class Person
+    public class Person
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
     }
 }
--- MVVMExample/ViewModels/MainViewModel.cs
+++ MVVMExample/ViewModels/MainViewModel.cs
@@ -1,12 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MVVMExample.Models;
+using MVVMExample.ViewModels.Base;
+using MVVMExample.ViewModels.Commands;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace MVVMExample.ViewModels
 {
-    class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        private string _firstName;
+        private string _lastName;
+        private int _age;
+
+        public MainViewModel()
+        {
+            People = new ObservableCollection<Person>();
+            AddPersonCommand = new RelayCommand(ExecuteAddPerson, CanExecuteAddPerson);
+        }
+
+        public ObservableCollection<Person> People { get; }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set => SetProperty(ref _firstName, value);
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set => SetProperty(ref _lastName, value);
+        }
+
+        public int Age
+        {
+            get => _age;
+            set => SetProperty(ref _age, value);
+        }
+
+        public ICommand AddPersonCommand { get; }
+
+        private bool CanExecuteAddPerson(object obj)
+        {
+            return !string.IsNullOrWhiteSpace(FirstName) &&
+                   !string.IsNullOrWhiteSpace(LastName) &&
+                   Age > 0;
+        }
+
+        private void ExecuteAddPerson(object obj)
+        {
+            People.Add(new Person
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Age = Age
+            });
+
+            // Clear the inputs
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Age = 0;
+        }
     }
 }
